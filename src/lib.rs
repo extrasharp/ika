b2ef08d89a3cf37ad210b391385f7d1c6c8ceaa2
extrasharp <genericpb@gmail.com>
2020-07-17 20:31:25 -0500

    clean up a little

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use std::{
 };
 
 // TODO write tests
+//      impl Recyclable on stdlib types
+//      make a threadsafe version
 
 pub trait Recyclable: Default {
     fn reset(&mut self) {
@@ -14,9 +16,31 @@ pub trait Recyclable: Default {
     }
 }
 
-// TODO impl Recyclable on stdlib types
+//
 
-// impl<T: Default> Recyclable for T {}
+// PoolObject.obj is completely safe
+//   always points to data in the pool
+//   all pointers from the pool are unique
+//   PoolObject must live as long as Pool
+//   needs to be a ptr to fit PoolObject::drop
+//     the PoolObject behind '&mut self' passed into drop will die afterwards
+//     rust complains self.obj pushed into the dead objects outlives the `&mut self` borrow
+//     which it does, in fact, do but. ya know.
+// RefCell accesses are completely safe
+//   RefCell is necessary so PoolObject doesnt need an &mut to the pool
+//     otherwise you could only do one pool.take()
+//     in this case its fine to have multiple mut refs to the pool
+//       they only ever modify one thing, pool.data
+//           and only do so for one funciton call
+//         its not thread safe, but otherwise its fine
+//       any 'two &mut's to the same data' issues are solved by PoolObject being safe
+//     Pool.take() being &self is a bonus
+//       because a pool is a basically a special memory allocator
+//         even if take() does mutate the pool, its not //really// mutating it
+//   could put a RefCell<Vec<*mut T>> in the PoolObject, but thats wierd
+//     doesnt solve the problem and also youd need a PhantomData<&'a Pool<T>> anyway
+
+//
 
 pub struct PoolObject<'a, T: Recyclable> {
     pool: &'a Pool<T>,
@@ -42,6 +66,8 @@ impl<'a, T: Recyclable> Drop for PoolObject<'a, T> {
         self.pool.dead.borrow_mut().push(self.obj);
     }
 }
+
+//
 
 pub struct Pool<T: Recyclable> {
     _data: Vec<T>,
